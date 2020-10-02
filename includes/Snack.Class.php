3153b32de131dfddbbb933d20e65736227ca86a2
Erpreet <preetkaurchd15@gmail.com>
2020-10-02 11:35:57 -0600

    number_format

---
+++ includes/Snack.Class.php
@@ -0,0 +1,21 @@
+<?php 
+
+Class Snack
+{
+    public $name = '';
+    public $type = '';
+    public $price = 0.00;
+    public $calories = 0;
+
+    function __construct ($snackName = '', $snackType = '', $snackPrice = 0.00, $snackCalories = 0)
+    {
+        $this->name = $snackName;
+        $this->type = $snackType;
+        $this->price = number_format($snackPrice, 2, '.',',');
+        $this->calories = $snackCalories;
+    }
+}
+
+$mySnack = new Snack ('Oh Henry', 'chocolate', 1.89, 200);
+var_dump($mySnack);
+
