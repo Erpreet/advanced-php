ce90bbb8b6ed3c79d614e454d6ad77920c1f2daa
Erpreet <preetkaurchd15@gmail.com>
2020-10-05 10:03:25 -0600

    Added page for external API

---
+++ cat-facts-api.php
@@ -0,0 +1,12 @@
+
+<?php
+// GLOBAL variables are stored in PHP's
+// $GLOBALS array.
+$GLOBALS['pageTitle'] = 'Cat Facts (External API test)';
+include './templates/header.php';
+?>
+
+
+
+<?php
+include './templates/footer.php';
